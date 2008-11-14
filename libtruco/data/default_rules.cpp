014179bda70d1c282229a8ecc2733df9f44216fc
ivancmartins <ivancmartins@60820942-988a-49fc-adfd-1d0f79145dba>
2008-11-14 16:19:54 +0000

    Conclusão da implementação da round_winner e mao_winner.
    git-svn-id: https://svn.code.sf.net/p/etruco/code/trunk@32 60820942-988a-49fc-adfd-1d0f79145dba

--- libtruco/data/default_rules.cpp
+++ libtruco/data/default_rules.cpp
@@ -4,9 +4,6 @@
 
 using namespace std;
 
-map <string, int> a;
-a["lala"] = 10;
-
 void give_cards( Rule& rule, Truco truco, vector< void * > params )
 {
 	truco.get_deck()->shuffle();
@@ -59,49 +56,40 @@ void play_card( Rule& rule, Truco truco, vector< void * > params ) 	//params[0] 
 		rule.set_state( "draw", 1 );
 }
 
-void round_winner( Rule& rule, Truco truco, vector< void * > params )//states: round, player1, player2, winner, who_is_player1, other_player
+void round_winner( Rule& rule, Truco truco, vector< void * > params )//states: round, team1, team2, winner, team1_points, team2_points
 {	
 	map <string, void*> state = rule.get_state();
 
-//pode ser feita em mao_winner sem o if?
-/*
 	if( state["round"]->value == 0 or state["winner"]->value != 0 )
 	{	
 		state["round"]->value = 5; 	//state["player1"] state["player2"] state["winner"]
 		state["winner"]->value = 0;
-		state["player1"]->value = 0;
-		state["player2"]->value = 0;
-		state["who_is_player"]->value = 0;
-		state["other_player"]->value = 0;
+		state["team1_points"]->value = 0;
+		state["team2_points"]->value = 0;
+		state["team1"]->value = 0;
+		state["team2"]->value = 0;
 	}
-*/
 
 	state["round"]->value--;
-	if( state["who_is_player1"]->value == 0 )
-		state["who_is_player1"]->value = rule.get_dependencies()["play_card"]->get_state("last_player");
-	else if( state["who_is_player1"]->value != rule.get_dependencies()["play_card"]->get_state("last_player") )
-		state["other_player"]->value = rule.get_dependencies()["play_card"]->get_state("last_player");
+	if( state["team1"]->value == 0 )
+		state["team1"]->value = rule.get_dependencies()["play_card"]->get_state("last_player");
+	else if( state["team1"]->value != rule.get_dependencies()["play_card"]->get_state("last_player") )
+		state["team2"]->value = rule.get_dependencies()["play_card"]->get_state("last_player");
+		
 	if( rule.get_dependencies()["play_card"]->get_state("draw") != 0 )
 	{
-		if( state["who_is_player1"]->value == rule.get_dependencies()["play_card"]->get_state("last_player") )
-			state["player1"]->value=state["player1"]->value+state["round"]->value
+		if( state["team1"]->value == rule.get_dependencies()["play_card"]->get_state("last_player") )
+			state["team1_points"]->value=state["team1"]->value+state["round"]->value;
 		else
-			state["player2"]->value=state["player2"]->value+state["round"]->value
+			state["team2_points"]->value=state["team2"]->value+state["round"]->value;
+			
+		state["winner"]->value=rule.get_dependencies()["play_card"]->get_state("last_player");
 	}
-
-//isso é do mao_winner
-/*
-	if( state["round"]->value == 3 )
+	else
 	{
-		if( state["player1"]->value == 7 )
-			state["winner"]->value = state["who_is_player"]->value;
-		if( state["player2"]->value == 7 )
-			state["winner"]->value = state["other_is_player"]->value;
-		bool draw = false;
-		if(( state["player1"]->value == 0 or state["player2"]->value == 0 ) and
-							 (state["player1"]->value != 7 or state["player2"]->value == 7 ))
-			draw = true;
-*/
+		state["winner"]->value=0;
+		state["draw"]->value=true;
+	}
 
 //tentativa anterior
 /*
@@ -119,9 +107,33 @@ void round_winner( Rule& rule, Truco truco, vector< void * > params )//states: r
 
 }
 
-void mao_winner( Rule& rule, Truco truco, vector< void * > params )
+void mao_winner( Rule& rule, Truco truco, vector< void * > params ) //states: winner, team1_points, team2_points
 {
-	rule.set_state( "winner", rule.get_dependencies()["round_winner"]->get_state("winner"));
+	//falta determinar quantos pontos a mão dá <depende da ask_truco>
+	map <string, void*> state = rule.get_state();
+	map <string, void*> state_round_winner = rule.get_dependencies()["round_winner"].get_state();
+	
+	//rule.set_state( "winner", rule.get_dependencies()["round_winner"]->get_state("winner"));
+
+	if( state_round_winner["round"]->value == 3 )
+	{
+		if( state_round_winner["team1_points"]->value == 7 )
+			state["winner"]->value = state_round_winner["team1"]->value;
+		if( state_round_winner["team2_points"]->value == 7 )
+			state["winner"]->value = state_round_winner["team2"]->value;
+		bool draw = false;
+	}
+	else if( state_round_winner["team1_points"]->value == 0 and state_round_winner["team2_points"]->value == 0 )
+			draw = true;
+	else if( state_round_winner["team1_points"]->value > state_round_winner["team2_points"]->value )
+			state["winner"]->value = state_round_winner["team1"]->value;
+		 else
+		 	state["winner"]->value = state_round_winner["team2"]->value;
+}
+
+void ask_truco( Rule& rule, Truco truco, vector< void * > params )
+{
+
 
 }
 
